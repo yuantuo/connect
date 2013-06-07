1075c679f8eeedfd83b6493e3ffb67e28f4f79f7
TOk <te.trade@hotmail.com>
2013-06-08 00:39:01 +0100

    first init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Connect::Application.config.secret_token = 'b39d52d2f38539fd8369907c74374febb89034f4c596c0d5cb59ff6fb9a5aab8ce7990a965aca7f429e31622c68d18280df3da043bade2ed8a873359aceb7c56'
